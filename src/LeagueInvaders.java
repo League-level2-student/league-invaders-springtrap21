f952ee60ad5d697726425ba1e6232ebcaef0d26a
springtrap21 <45442135+springtrap21@users.noreply.github.com>
2020-09-16 19:30:06 -0700

    InvadersDay1

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 500;
+	LeagueInvaders() {
+		frame = new JFrame();
+	}
+	
+public static void main(String[] args) {
 
+	
+	
+}
 }
